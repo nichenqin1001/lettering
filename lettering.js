a432500bade1795beb434ecea0fef14859961f4b
nichenqin1001 <nichenqin@hotmail.com>
2017-06-16 20:47:31 +0800

    config babel & throw error if no elment provided

--- lettering.js
+++ lettering.js
@@ -1,1 +1,21 @@
-class Lettering { }
+const rAF = window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.oRequestAnimationFrame ||
+  window.msRequestAnimationFrame ||
+  function (callback) { window.setTimeout(callback, 1000 / 60); };
+
+// define cancelAnimationFrame function
+const cAF = window.cancelAnimationFrame || window.webkitCancelAnimationFrame;
+
+const x = p => { throw new Error(`Missing Parameter: ${p}`) }
+
+class Lettering {
+  constructor(el = x`el`, options) {
+    this.el = typeof el === 'string' ? document.querySelector(el) : el;
+
+    this.string = this.el.textContent;
+  }
+}
+
+export default Lettering;
