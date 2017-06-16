32073060dc8294fc55f0afe417f1732584f6fdfb
nichenqin1001 <nichenqin@hotmail.com>
2017-06-16 22:11:32 +0800

    animation works

--- lettering.js
+++ lettering.js
@@ -10,12 +10,63 @@ const cAF = window.cancelAnimationFrame || window.webkitCancelAnimationFrame;
 
 const x = p => { throw new Error(`Missing Parameter: ${p}`) }
 
+const defaultOptions = {
+  speed: 150
+}
+
+const removeChild = el => {
+  while (el.firstChild) {
+    el.removeChild(el.firstChild);
+  }
+}
+
 class Lettering {
-  constructor(el = x`el`, options) {
+  /**
+   * Creates an instance of Lettering.
+   * @param {HTMLElement} el 
+   * @param {object} options 
+   * 
+   * @memberof Lettering
+   */
+  constructor(el = x`el`, options = {}) {
+    // init type element
     this.el = typeof el === 'string' ? document.querySelector(el) : el;
+    // init options
+    this.options = Object.assign({}, defaultOptions, options)
 
     this.string = this.el.textContent;
+
+    this.stringArrLike = Array.prototype.slice.call(this.string);
+
+    this.stringIndex = 0;
+
+    this.print = this.print.bind(this);
+
+    this._init();
   }
+
+  _init() {
+    removeChild(this.el);
+
+    this.print();
+  }
+
+  _renderText(num) {
+    this.el.appendChild(document.createTextNode(this.stringArrLike[num]));
+  }
+
+  print() {
+    this._renderText(this.stringIndex);
+
+    this.stringIndex++;
+
+    if (this.stringIndex < this.stringArrLike.length) return this.requestId = rAF(this.print);
+  }
+
+  back() {
+
+  }
+
 }
 
 export default Lettering;
