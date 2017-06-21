ec6a8f66bb89f9d930f20324d11212e314d3b2a4
nichenqin1001 <nichenqin@hotmail.com>
2017-06-21 12:20:36 +0800

    event emitter test

--- test/lettring.test.js
+++ test/lettring.test.js
@@ -1,5 +1,33 @@
 describe('Lettering Library', () => {
-  it('test jasmine', () => {
-    expect(true).toBe(true);
+
+  it('should create an instance of Lettering', () => {
+    expect(lettering).toBeDefined();
+    expect(letteringInput).toBeDefined();
   });
+
+
+  describe('Event Emitter', () => {
+
+    describe('addListener()', () => {
+
+      var testTxt;
+
+      beforeEach((done) => {
+        testTxt = '';
+        lettering.addListener('afterTyping', () => {
+          testTxt = 'afterTyping';
+          done();
+        });
+      });
+
+      it('should do something after add afterTyping event', () => {
+        expect(testTxt).toEqual('afterTyping');
+      });
+
+    });
+
+
+  });
+
+
 });
