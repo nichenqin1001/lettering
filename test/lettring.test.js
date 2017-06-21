0aa8d1d89d076a477b28e3d709e1494c00659dc1
nichenqin1001 <nichenqin@hotmail.com>
2017-06-21 13:49:23 +0800

    use mocha to test event emitter

--- test/lettring.test.js
+++ test/lettring.test.js
@@ -1,10 +1,19 @@
+var expect = chai.expect;
+var should = chai.should();
+
+var test = document.getElementById('test');
+var input = document.getElementById('input');
+
+var lettering = new Lettering('#test');
+console.log(lettering);
+var letteringInput = new Lettering('#test-input');
+console.log(letteringInput);
+
 describe('Lettering Library', () => {
 
   it('should create an instance of Lettering', () => {
-    expect(lettering).toBeDefined();
-    expect(letteringInput).toBeDefined();
+    should.exist(lettering);
   });
-
 
   describe('Event Emitter', () => {
 
@@ -12,22 +21,35 @@ describe('Lettering Library', () => {
 
       var testTxt;
 
-      beforeEach((done) => {
+      beforeEach(() => {
         testTxt = '';
+      });
+
+      it('should do something after type end', (done) => {
         lettering.addListener('afterTyping', () => {
           testTxt = 'afterTyping';
           done();
+          expect(testTxt).to.be.equal('afterTyping');
         });
-      });
+      }).timeout(5000);
 
-      it('should do something after add afterTyping event', () => {
-        expect(testTxt).toEqual('afterTyping');
-      });
+      it('should do something after backspace', (done) => {
+        lettering.backspace();
+        lettering.addListener('afterBackspace', () => {
+          testTxt = 'afterBackspace';
+          done();
+          expect(testTxt).to.be.equal('afterBackspace');
+        });
+      }).timeout(5000);
 
     });
 
 
   });
+
+  describe('chack input', () => {
+
+  })
 
 
 });
